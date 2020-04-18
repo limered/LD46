71395150a373aaf0b5f501898fc864437a2f3826
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-04-18 23:23:35 +0200

    #9 add key press system

---
+++ KackeIsAtTheDampf/Assets/Systems/Key/KeyPressSystem.cs
@@ -0,0 +1,68 @@
+﻿using Assets.Systems.Beat;
+using System;
+using System.Linq;
+using SystemBase;
+using UniRx;
+using UnityEngine;
+
+namespace Assets.Systems.Key
+{
+    [GameSystem(typeof(BeatSystem))]
+    public class KeyPressSystem : GameSystem<BeatSystemConfig>
+    {
+        private PressedKey _lastPressedKey;
+        private float _greenThreshold = 20f;
+        private float _yellowThreshold = 60f;
+        private readonly string[] _relevantKeys = { "r", "t", "y", "u", "f", "g", "h", "j", "v", "b", "n", "m" };
+
+        public override void Register(BeatSystemConfig component)
+        {
+            component.BeatTrigger
+                .Subscribe(OnBeat)
+                .AddTo(component);
+
+            SystemUpdate()
+                .Subscribe(OnUpdate)
+                .AddTo(component);
+        }
+
+        private void OnUpdate(float obj)
+        {
+            if (!Input.anyKeyDown) return;
+
+            var input = Input.inputString;
+            if (input.Length == 1 && _relevantKeys.Contains(input))
+            {
+                _lastPressedKey = new PressedKey
+                {
+                    Key = input,
+                    Timestamp = Time.frameCount
+                };
+            }
+        }
+
+        private void OnBeat(Unit _)
+        {
+            var beatTime = Time.frameCount;
+            var timeDiff = Math.Abs(beatTime - _lastPressedKey.Timestamp);
+            if (timeDiff < _greenThreshold)
+            {
+                Debug.Log("green " + timeDiff);
+            }
+            else if (timeDiff < _yellowThreshold)
+            {
+                Debug.Log("yellow " + timeDiff);
+            }
+            else
+            {
+                Debug.Log(timeDiff);
+            }
+        }
+
+        public struct PressedKey
+        {
+            public string Key { get; set; }
+            public float Timestamp { get; set; }
+        }
+    }
+}
