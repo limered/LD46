122a1a1d77bb77a6283e0c27cc09bf21099c2c0c
Michael Scherbakow <ryupold@gmail.com>
2020-04-18 22:45:00 +0200

    #7 added basic hypemeter sprite

---
+++ KackeIsAtTheDampf/Assets/Systems/Hype/HypeReactorComponent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameState.States;
+using SystemBase;
+using SystemBase.StateMachineBase;
+using Systems.GameState.Messages;
+using StrongSystems.Audio.Helper;
+using UniRx;
+using Utils;
+using System;
+
+namespace Systems.Hype
+{
+    public class HypeReactorComponent : GameComponent
+    {
+        
+    }
+}
