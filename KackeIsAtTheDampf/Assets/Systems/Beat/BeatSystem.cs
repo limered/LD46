bdc43af534d796f4655782e5944f336f0d66e4fe
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-04-18 17:40:37 +0200

    #9 add beat system

---
+++ KackeIsAtTheDampf/Assets/Systems/Beat/BeatSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using SystemBase;
+using UniRx;
+using UnityEngine;
+
+namespace Assets.Systems.Beat
+{
+    [GameSystem]
+    public class BeatSystem : GameSystem<BeatSystemConfig>
+    {
+        private IDisposable _timerDisposable;
+
+        public override void Register(BeatSystemConfig component)
+        {
+            component.BPM.Subscribe(bpm => BPMChanged(bpm, component));
+        }
+
+        private void BPMChanged(float bpm, BeatSystemConfig config)
+        {
+            var timePerBeat = 60f / bpm;
+            _timerDisposable?.Dispose();
+            _timerDisposable = Observable
+                .Interval(TimeSpan.FromSeconds(timePerBeat))
+                .Subscribe(_ => OnBeat(config));
+        }
+
+        private void OnBeat(BeatSystemConfig cofig)
+        {
+            cofig.BeatTrigger.ForceExecute();
+        }
+    }
+}
---
+++ KackeIsAtTheDampf/Assets/Systems/Beat/BeatSystemConfig.cs
@@ -0,0 +1,12 @@
+﻿using SystemBase;
+using UniRx;
+
+namespace Assets.Systems.Beat
+{
+    public class BeatSystemConfig : GameComponent
+    {
+        public FloatReactiveProperty BPM = new FloatReactiveProperty(120);
+
+        public ReactiveCommand BeatTrigger = new ReactiveCommand();
+    }
+}
