6a2f27b26ffc1eb49ac3f6c24a9eb63b0e9e5904
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-04-19 02:33:01 +0200

    #9 change key checker to emit event

--- KackeIsAtTheDampf/Assets/Systems/Key/KeyPressSystem.cs
+++ KackeIsAtTheDampf/Assets/Systems/Key/KeyPressSystem.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Systems.Beat;
-using System;
+﻿using Assets.Systems.Key.Events;
 using System.Linq;
 using SystemBase;
 using UniRx;
@@ -7,20 +6,13 @@ using UnityEngine;
 
 namespace Assets.Systems.Key
 {
-    [GameSystem(typeof(BeatSystem))]
-    public class KeyPressSystem : GameSystem<BeatSystemConfig>
+    [GameSystem]
+    public class KeyPressSystem : GameSystem<KeyInfoComponent>
     {
-        private PressedKey _lastPressedKey;
-        private float _greenThreshold = 20f;
-        private float _yellowThreshold = 60f;
         private readonly string[] _relevantKeys = { "r", "t", "y", "u", "f", "g", "h", "j", "v", "b", "n", "m" };
 
-        public override void Register(BeatSystemConfig component)
+        public override void Register(KeyInfoComponent component)
         {
-            component.BeatTrigger
-                .Subscribe(OnBeat)
-                .AddTo(component);
-
             SystemUpdate()
                 .Subscribe(OnUpdate)
                 .AddTo(component);
@@ -33,36 +25,12 @@ namespace Assets.Systems.Key
             var input = Input.inputString;
             if (input.Length == 1 && _relevantKeys.Contains(input))
             {
-                _lastPressedKey = new PressedKey
+                MessageBroker.Default.Publish(new EvtKeyPressed
                 {
                     Key = input,
-                    Timestamp = Time.frameCount
-                };
+                    Timestamp = Time.realtimeSinceStartup
+                });
             }
-        }
-
-        private void OnBeat(Unit _)
-        {
-            var beatTime = Time.frameCount;
-            var timeDiff = Math.Abs(beatTime - _lastPressedKey.Timestamp);
-            if (timeDiff < _greenThreshold)
-            {
-                Debug.Log("green " + timeDiff);
-            }
-            else if (timeDiff < _yellowThreshold)
-            {
-                Debug.Log("yellow " + timeDiff);
-            }
-            else
-            {
-                Debug.Log(timeDiff);
-            }
-        }
-
-        public struct PressedKey
-        {
-            public string Key { get; set; }
-            public float Timestamp { get; set; }
         }
     }
 }
