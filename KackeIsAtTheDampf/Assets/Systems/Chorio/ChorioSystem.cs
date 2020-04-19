1a7cdd34e4eaf3f4ecb58b186acb5b1db11e1c9f
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-04-19 02:32:37 +0200

    #9 add basic chorio system

---
+++ KackeIsAtTheDampf/Assets/Systems/Chorio/ChorioSystem.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Systems.Beat;
+using Assets.Systems.Chorio.Evt;
+using SystemBase;
+using UniRx;
+
+namespace Assets.Systems.Chorio
+{
+    [GameSystem(typeof(BeatSystem))]
+    public class ChorioSystem : GameSystem<BeatSystemConfig>
+    {
+        public override void Register(BeatSystemConfig component)
+        {
+            component.BeatTrigger
+                .Subscribe(OnBeat)
+                .AddTo(component);
+        }
+
+        private void OnBeat(int beatNo)
+        {
+            if (beatNo % 4 != 0) return;
+
+            MessageBroker.Default.Publish(new EvtNextBeatKeyAdded
+            {
+                Key = "g",
+                BeatNo = beatNo + 2,
+            });
+        }
+    }
+}
---
+++ KackeIsAtTheDampf/Assets/Systems/Chorio/Evt/EvtNextBeatKeyAdded.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Systems.Chorio.Evt
+{
+    public class EvtNextBeatKeyAdded
+    {
+        public int BeatNo { get; set; }
+        public string Key { get; set; }
+    }
+}
