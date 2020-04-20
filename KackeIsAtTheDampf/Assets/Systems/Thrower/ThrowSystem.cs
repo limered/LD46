f4a79b231c5a07720cf612826614c3dcf9baba99
HelenBohleber <helen-bohleber@web.de>
2020-04-20 23:47:15 +0200

    #43 add throw system

---
+++ KackeIsAtTheDampf/Assets/Systems/Thrower/ThrowSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowSystem : MonoBehaviour
+{
+    void Start()
+    {
+        var throwComponent = gameObject.GetComponent<ThrowComponent>();
+        throwComponent.ThrowTypes[0].gameObject.SetActive(true);
+        throwComponent.ThrowTypes[1].gameObject.SetActive(false);
+        throwComponent.ThrowTypes[2].gameObject.SetActive(false);
+    }
+}
