060a480705289b1683515ed1e2c5dd356616d1a7
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2020-04-20 18:28:07 +0200

    test version without f

--- KackeIsAtTheDampf/Assets/Systems/Key/KeyInfoComponent.cs
+++ KackeIsAtTheDampf/Assets/Systems/Key/KeyInfoComponent.cs
@@ -4,7 +4,8 @@ namespace Assets.Systems.Key
 {
     public class KeyInfoComponent : GameComponent
     {
-        public string[] RelevantKeys = { "w", "e", "r", "s", "d", "f", "x", "c", "v" };
+        //public string[] RelevantKeys = { "w", "e", "r", "s", "d", "f", "x", "c", "v" };
+        public string[] RelevantKeys = { "w", "e", "r", "s", "d", "x", "c", "v" };
 
     }
 }
